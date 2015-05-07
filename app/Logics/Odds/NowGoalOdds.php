9e7d4ce8c798393a0d4cd75fd6c8aef0240297e0
Michael <hungnv038@gmail.com>
2015-05-07 10:18:29 +0700

    added code to get v9bet data

--- app/Logics/Odds/NowGoalOdds.php
+++ app/Logics/Odds/NowGoalOdds.php
@@ -12,7 +12,7 @@ namespace App\Logics\Odds;
 use App\Logics\base\OddServiceBase;
 
 class NowGoalOdds extends OddServiceBase{
-    public function processData()
+    public function processData($matchs=null,$match_odds=null)
     {
         // TODO: Implement processData() method.
     }
--- app/Logics/Odds/V9BetOdds.php
+++ app/Logics/Odds/V9BetOdds.php
@@ -9,12 +9,37 @@
 namespace App\Logics\Odds;
 
 
+use App\Libraries\Constants;
 use App\Logics\base\OddServiceBase;
+use App\Models\Odds;
 
 class V9BetOdds extends OddServiceBase {
-    public function processData()
+    public function processData($matchs=null,$match_odds=null)
     {
-        // TODO: Implement processData() method.
+        $odd_types=array(
+            '1x2'   =>array('home'=>1,'draw'=>5,'away'=>3,'type'=>Constants::ODD_1X2),
+            '1x21st'=>array('home'=>1,'draw'=>5,'away'=>3,'type'=>Constants::ODD_1X21ST),
+            'ah'    =>array('home'=>5,'draw'=>3,'away'=>7,'type'=>Constants::ODD_AH),
+            'ah1st' =>array('home'=>5,'draw'=>3,'away'=>7,'type'=>Constants::ODD_AH1ST),
+            'ou'    =>array('home'=>5,'draw'=>3,'away'=>7,'type'=>Constants::ODD_OU),
+            'ou1st' =>array('home'=>5,'draw'=>3,'away'=>7,'type'=>Constants::ODD_OU1ST),
+        );
+        $odd_objs=array();
+        foreach ($match_odds as $match_id=> $odds) {
+            if(!is_array($odds)) $odds=(array)$odds;
+            foreach ($odd_types as $key=> $config) {
+                if(isset($odds[$key])) {
+                    $home=floatval($odds[$key][$config['home']]);
+                    $away=floatval($odds[$key][$config['away']]);
+                    $draw=floatval($odds[$key][$config['draw']]);
+                    $odd_objs[]=Odds::makeObject($matchs[$match_id],$home,$draw,$away,$config['type']);
+                }
+            }
+        }
+
+        // insert Odd to table
+        $oddModel=new Odds();
+        $oddModel->batchInsert(array_values($odd_objs));
     }
 
 }
