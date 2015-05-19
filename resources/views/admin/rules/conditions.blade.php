9372a0c636eb7dc39df3df1735d6d763bdd18610
hieutrieu <mrhieutrieu@gmail.com>
2015-05-19 09:43:09 +0700

    A

--- resources/views/admin/rules/conditions.blade.php
+++ resources/views/admin/rules/conditions.blade.php
@@ -1,13 +1,13 @@
 <?php use \App\Libraries\InputHelper; ?>
-<div class="box box-solid box-warning">
+<div class="box box-solid box-warning widget-table">
     <div class="box-header">
         <h3 class="box-title"><i class="fa fa-list"></i> {{Lang::get('app.conditions')}}</h3>
         <div class="box-tools pull-right" role="group" aria-label="...">
             <button type="button" class="btn btn-sm btn-warning" onclick="RuleModule.addRule(this)"><span class="fa fa-plus"></span> {{Lang::get('app.create_condition_new')}}</button>
         </div>
     </div>
-    <div class="box-body">
-        <table class="table table-striped table-bordered table-responsive">
+    <div class="box-body widget-content">
+        <table class="table table-striped table-bordered table-hover table-responsive">
             <tr class="active" style="background-color: #2a6496;">
                 <th style="text-align: center; width:15%;">{{Lang::get('app.name')}}</th>
                 <th style="width:30%;">{{Lang::get('app.description')}}</th>
